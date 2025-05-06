754a5682ecb2c9a6bff9c18589ce73cacd844190
ankita-masal1304 <ankitamasal3@gmail.com>
2025-05-06 10:37:45 +0530

    variable Assignment 01
    variable Assignment 01

---
+++ 01-varable-assignment.js
@@ -0,0 +1,45 @@
+  console.log("--before initialization--");
+  
+  var myFullName;
+  console.log("myFullName:",myFullName);
+
+  console.log("--After inititialization--");
+  var myFullName = "Ankita Masal";
+  console.log("myFullName:",myFullName);
+
+  console.log("--Update--");
+  var myFullName = "Ankita Mohan Masal";
+  console.log("myFullName:",myFullName);
+  
+  console.log("--Declare and initialization--");
+  var myLove ="I Love Only JS";
+  console.log("I Love Only JS:",myLove);
+
+  console.log("--declare and initalization must on same line");
+  var firstName ="Ankita";
+  console.log("firstName:",firstName);
+  var lastName = "Masal";
+  console.log("Last Name :",lastName);
+  var age ="21";
+  console.log("Age is a :",age);
+  var address = "Mumbai";
+  console.log("Address is a :",address);
+  var pinCode ="400101";
+  console.log("PIN Code is:",pinCode);
+  var gender ="female";
+  console.log("Gender is a :",gender);
+  var isMarried ="false";
+  console.log("isMarried:",isMarried);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
---
+++ 02-data-type.js
@@ -0,0 +1,24 @@
+
+console.log("===primative Dt===");
+var city ="Mumbai";
+ var result = typeof city;
+ console.log("city:",city,"type of city:",result);
+ 
+
+var name ="ankita";
+var she =typeof name;
+console.log("Nama is a :",name,"its type of:",she);
+
+var code ="413310";
+ var pin = typeof code;
+console.log("Pin Code is:",code,"The Code is:",pin);
+
+console.log("==========================");
+var age = 20;
+var typeOfAge = typeof age;
+console.log("Age: ", age, " and it's data type is: ", typeOfAge);
+
+console.log("==========================");
+var isMarried = false;
+var typeOfIsMarried = typeof isMarried;
+console.log("Is Married: ", isMarried, " and it's DT is: ", typeOfIsMarried);
