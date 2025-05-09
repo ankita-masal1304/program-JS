63bdbe2424d4848b1d46f933b17af80a7a5692ec
ankita-masal1304 <ankitamasal3@gmail.com>
2025-05-09 10:48:21 +0530

    string Assig 01 is done
    string Assig 01 is done

---
+++ 03-strinngBasicAssig01.js
@@ -0,0 +1,14 @@
+
+function stringBasics(){
+    var dream = "Apple";
+    console.log(`my dream company is:${dream}`);
+    var hobby1 = "reading Book";
+    var hobby2 = "listen music";
+    var hobby3 = "riding bike";
+    console.log(`My Hobbies are:${hobby1+hobby2+hobby3}`);
+    var total = hobby1.length+hobby2.length+hobby3.length;
+    console.log(`sum of a number in char:${total}`);
+    
+    
+}
+stringBasics();
